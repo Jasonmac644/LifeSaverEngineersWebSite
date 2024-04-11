5aab2a93f4179ee51d5b373f2369647344c517d1
Jasonmac644 <jasonmacdonald644@yahoo.com>
2024-04-10 21:19:17 -0400

    fixed gmail Login
    UseEffect is used for lifecycle of redirectresults for google sign in

--- software-frontend/lifesaverengineers/src/hooks/contexts/AuthContext.js
+++ software-frontend/lifesaverengineers/src/hooks/contexts/AuthContext.js
@@ -1,85 +1,58 @@
 import { createContext, useState, useEffect } from "react";
 import {
-	getAuth,
-	onAuthStateChanged,
-	signInWithEmailAndPassword,
-	signOut,
-	createUserWithEmailAndPassword,
-	GoogleAuthProvider,
-	signInWithRedirect,
-	getRedirectResult,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  getRedirectResult,
 } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 import firebaseConfig from "../../firebase-config";
-import { useNavigate } from "react-router-dom";
 export const Context = createContext();
 
 // Create a provider component
 export const FirebaseProvider = ({ children }) => {
-	const app = initializeApp(firebaseConfig);
-	const auth = getAuth(app);
-	const db = getFirestore(app);
-	const provider = new GoogleAuthProvider();
-	const [user, setUser] = useState(null);
-	const [loading, setLoading] = useState(true);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	const logOut = () => {
-		setLoading(true);
-		return signOut(auth);
-	};
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-	useEffect(() => {
-		let unsubscribe;
-		unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-			setLoading(false);
-			if (currentUser) setUser(currentUser);
-			else setUser(null);
-		});
-		return () => {
-			if (unsubscribe) unsubscribe();
-		};
-	}, []);
+  useEffect(() => {
+    let unsubscribe;
+    unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoading(false);
+      if (currentUser) setUser(currentUser);
+      else setUser(null);
+    });
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+  }, []);
 
-	//Google Sign In
-	const initiateGoogleSignIn = async () => {
-		try {
-			await signInWithRedirect(auth, provider);
-		} catch (error) {
-			console.error("Error initiating sign-in with Google:", error);
-		}
-	};
-	const processSignInRedirectResult = async () => {
-		try {
-			const result = await getRedirectResult(auth);
-			if (result) {
-				// User signed in successfully
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				const token = credential.accessToken;
-				const user = result.user;
-				if (user) {
-					return true;
-				}
-				// Navigate to dashboard or perform other actions
-			}
-		} catch (error) {
-			// Handle errors from the redirect result
-			console.error("Error processing redirect result:", error);
-			return false;
-		}
-	};
-	const firebaseValue = {
-		user: user,
-		db: db,
-		setUser: setUser,
-		logOut: logOut,
-		initiateGoogleSignIn: initiateGoogleSignIn,
-		processSignInRedirectResult: processSignInRedirectResult,
-	};
 
-	return (
-		<Context.Provider value={firebaseValue}>
-			{!loading && children}
-		</Context.Provider>
-	);
+
+  const firebaseValue = {
+    user: user,
+    db: db,
+    auth: auth,
+    setUser: setUser,
+    logOut: logOut,
+  };
+
+  return (
+    <Context.Provider value={firebaseValue}>
+      {!loading && children}
+    </Context.Provider>
+  );
 };
--- software-frontend/lifesaverengineers/src/pages/auth/Login.js
+++ software-frontend/lifesaverengineers/src/pages/auth/Login.js
@@ -1,8 +1,10 @@
 import { useState, useContext, useEffect } from "react";
 import {
-	signInWithEmailAndPassword,
-	GoogleAuthProvider,
-	getAuth,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+  getRedirectResult,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { Container } from "@mui/material";
@@ -16,96 +18,105 @@ import GoogleButton from "react-google-button";
 import { Context } from "../../hooks/contexts/AuthContext";
 
 const Login = () => {
-	const navigate = useNavigate();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const provider = new GoogleAuthProvider();
-	const auth = getAuth();
-	const { initiateGoogleSignIn, processSignInRedirectResult } =
-		useContext(Context);
-	const onLogin = (e) => {
-		e.preventDefault();
-		signInWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				navigate("/dashboard");
-				console.log(user);
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				console.log(errorCode, errorMessage);
-			});
-	};
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const provider = new GoogleAuthProvider();
+  const { auth } = useContext(Context);
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/dashboard");
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-	const handleEmailChange = (e) => {
-		setEmail(e.target.value);
-	};
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-	const handlePasswordChange = (e) => {
-		setPassword(e.target.value);
-	};
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-	const handleGoogleSignIn = () => {
-		async () => {
-			await initiateGoogleSignIn().then(() => {
-				processSignInRedirectResult();
-			});
-		};
-	};
+  const handleGoogleSignIn = async () => {
+    await signInWithRedirect(auth, provider);
+  };
 
-	return (
-		<Container
-			sx={{
-				backgroundColor: appColor.gray,
-				minHeight: "100vh",
-				minWidth: "100%",
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				flexDirection: "column",
-			}}
-		>
-			<Stack direction={"column"} mb={"10vh"}>
-				<img
-					src={MainLogo}
-					alt="Main Logo"
-					height="300px"
-					style={{ marginBottom: "40px" }}
-				/>
-				<FormControl variant="standard">
-					{/* <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} /> */}
-					<TextField
-						required
-						id="email"
-						label="Email"
-						variant="filled"
-						value={email}
-						onChange={handleEmailChange}
-						style={{ marginBottom: "5px" }}
-					/>
-					<TextField
-						required
-						id="password"
-						label="Password"
-						variant="filled"
-						type="password"
-						onChange={handlePasswordChange}
-						value={password}
-					/>
-				</FormControl>
-				<Button sx={{ mt: "20px" }} variant="contained" onClick={onLogin}>
-					Login
-				</Button>
-				<GoogleButton
-					type="dark"
-					onClick={handleGoogleSignIn}
-					style={{ width: "100%", marginTop: "20px" }}
-				/>
-			</Stack>
-		</Container>
-	);
+  useEffect(() => {
+    if (auth) {
+      getRedirectResult(auth)
+        .then((result) => {
+          if (result) {
+            navigate("/dashboard");
+			console.log(result)
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, []);
+
+  return (
+    <Container
+      sx={{
+        backgroundColor: appColor.gray,
+        minHeight: "100vh",
+        minWidth: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Stack direction={"column"} mb={"10vh"}>
+        <img
+          src={MainLogo}
+          alt="Main Logo"
+          height="300px"
+          style={{ marginBottom: "40px" }}
+        />
+        <FormControl variant="standard">
+          {/* <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} /> */}
+          <TextField
+            required
+            id="email"
+            label="Email"
+            variant="filled"
+            value={email}
+            onChange={handleEmailChange}
+            style={{ marginBottom: "5px" }}
+          />
+          <TextField
+            required
+            id="password"
+            label="Password"
+            variant="filled"
+            type="password"
+            onChange={handlePasswordChange}
+            value={password}
+          />
+        </FormControl>
+        <Button sx={{ mt: "20px" }} variant="contained" onClick={onLogin}>
+          Login
+        </Button>
+        <GoogleButton
+          type="dark"
+          onClick={handleGoogleSignIn}
+          style={{ width: "100%", marginTop: "20px" }}
+        />
+      </Stack>
+    </Container>
+  );
 };
 
 export default Login;
