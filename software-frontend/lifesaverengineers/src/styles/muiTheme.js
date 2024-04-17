66a187f4e0a460f87d8156c8f1bbedc3c595c077
Jason <jasonmacdonald644@yahoo.com>
2024-04-16 22:40:27 -0400

    Login fixed

--- software-frontend/lifesaverengineers/src/styles/muiTheme.js
+++ software-frontend/lifesaverengineers/src/styles/muiTheme.js
@@ -1,45 +1,64 @@
 import { createTheme } from "@mui/material/styles";
 import appColor from "./colors";
 const theme = createTheme({
-  typography: {
-  },
-  components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          color: "red",
-        },
-      },
-    },
-    MuiListItemButton: {
-      styleOverrides: {
-        root: {
-          "&.Mui-selected": {
-            backgroundColor: appColor.ashGreenTint,
-          },
-          ":hover": {
-            backgroundColor: appColor.ashGreenTint,
-          },
-          "&.Mui-selected:hover": {
-            backgroundColor: appColor.ashGreenTint,
-          },
-        },
-      },
-    },
-    MuiPickersDay: {
-      styleOverrides: {
-        root: {
-          '&.Mui-selected.Mui-selected': {
-            backgroundColor: appColor.fawn,
-            '&:hover': {
-              backgroundColor: '#ff8787',
-            }
-          },
-        }
-      }
-    }
-  
-  },
+	typography: {},
+	components: {
+		MuiTextField: {
+			styleOverrides: {
+				root: {
+					color: "black",
+					width: "100%",
+				},
+			},
+		},
+
+		MuiFilledInput: {
+			styleOverrides: {
+				root: {
+					"&:focused": {
+						backgroundColor: "rgba(0,0,0,0.6)",
+					},
+					"&::after": {
+						borderBottom: `2px solid ${appColor.ashGreenTint}`,
+					},
+				},
+			},
+		},
+		MuiInputLabel: {
+			styleOverrides: {
+				root: {
+					"&.Mui-focused": { color: "black" },
+				},
+			},
+		},
+		MuiListItemButton: {
+			styleOverrides: {
+				root: {
+					"&.Mui-selected": {
+						backgroundColor: appColor.ashGreenTint,
+					},
+					":hover": {
+						backgroundColor: appColor.ashGreenTint,
+					},
+					"&.Mui-selected:hover": {
+						backgroundColor: appColor.ashGreenTint,
+					},
+				},
+			},
+		},
+		MuiPickersDay: {
+			styleOverrides: {
+				root: {
+					"&.Mui-selected.Mui-selected": {
+						backgroundColor: appColor.fawn,
+						"&:hover": {
+							backgroundColor: "#ff8787",
+						},
+					},
+				},
+			},
+		},
+	},
 });
 
 export default theme;
